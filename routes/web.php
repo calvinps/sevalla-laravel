498dea73dcf4a485c675c9e8cb9c4721b7700ef4
Calvin Schemanski <calvinps@umich.edu>
2025-01-14 13:57:07 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,42 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+function cpuUsage(int $sec): void {
+    $start = microtime(true);
+    while ((microtime(true) - $start) < $sec) {
+        sqrt(rand());
+    }
+}
+
+function memUsage(int $mb): void {
+    $allocated= [];
+    for ($i = 0; $i < $mb; $i++) {
+        $allocated[] = str_repeat('A', 1024 * 1024);
+    }
+    defer(function(){
+      sleep(2);
+      unset($allocated);
+    });
+}
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/cpu', function (Request $request) {
+  defer(function(){
+    $sec = (int) $request->query('sec', 2);
+    cpuUsage($sec);
+  });
+
+  return response()->json(['message' => 'CPU']);
+});
+
+Route::get('/mem', function (Request $request) {
+  $mb = (int) $request->query('mb', 50);
+  memUsage($mb);
+
+  return response()->json(['message' => "Memory"]);
 });
