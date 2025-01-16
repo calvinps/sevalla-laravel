f94e34e9137d2fe55db273a0d12f4589d78b97da
Calvin Schemanski <calvinps@umich.edu>
2025-01-16 04:58:29 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-function cpuUsage(int $sec): void {
+function cpuUsage(int $iter): string {
     $start = microtime(true);
-    while ((microtime(true) - $start) < $sec) {
-        sqrt(rand());
+    $res  = [];
+    $num  = [1723, 4856, 9021, 3478, 6215, 7384, 2549, 8102, 4967, 3690];
+    for ($i = 0; $i < $iter; $i++) {
+        $idx = $i % count($num);
+        sqrt($num[$idx]);
     }
+
+    return $iter.' iterations took '.microtime(true) - $start.' seconds';
+}
+
+function cpuHash(int $iter, string $algo = 'sha512'): string {
+    // Arbitrary key and salt; you can modify these as needed.
+    $key  = 'ABCDEFG12345678ABCDEFG12345678ABCDEFG12345678ABCDEFG12345678';
+    $salt = 'HIJKLMN87654321HIJKLMN87654321HIJKLMN87654321HIJKLMN87654321';
+    $start = microtime(true);
+    hash_pbkdf2($algo, $key, $salt, $iter, 64);
+    return 'took '.microtime(true) - $start.' seconds';
 }
 
 function memUsage(int $mb): void {
@@ -26,12 +40,10 @@ Route::get('/', function () {
 });
 
 Route::get('/cpu', function (Request $request) {
-  defer(function(){
-    $sec = (int) $request->query('sec', 2);
-    cpuUsage($sec);
-  });
-
-  return response()->json(['message' => 'CPU']);
+  $iter = (int) $request->query('iter', 1000000);
+  $method = (string) $request->query('method', 'cpuHash');
+  $res = $method($iter);
+  return response()->json(['message' => 'CPU', 'res' => $res]);
 });
 
 Route::get('/mem', function (Request $request) {
